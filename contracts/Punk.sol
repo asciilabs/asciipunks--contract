c107e3d38cfb1b91722cdd718063fb8352771fad
Nassredean Nasseri <dean@nasseri.io>
2021-03-06 18:37:43 -0500

    better test

--- contracts/Punk.sol
+++ contracts/Punk.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
-
+pragma solidity ^0.7.6;
 pragma abicoder v2;
 
 contract Punk {
-  uint internal constant ROW_LENGTH = 3;
-  uint internal constant ROW_COUNT = 3;
+  uint internal constant ROW_LENGTH = 12;
+  uint internal constant ROW_COUNT = 12;
+  uint internal constant MOUTH_LENGTH = ROW_LENGTH;
+
+  uint internal constant MOUTH_ROW = 7;
 
   function splice(
     string[ROW_LENGTH * ROW_COUNT] memory rows,
@@ -16,5 +19,44 @@ contract Punk {
     }
 
     return rows;
+  }
+
+  function drawMouth(
+    string[ROW_LENGTH * ROW_COUNT] memory rows, 
+    string[MOUTH_LENGTH] memory mouth
+  ) public pure returns (string[ROW_LENGTH * ROW_COUNT] memory) {
+    string[ROW_LENGTH * ROW_COUNT] memory res = splice(rows, mouth, MOUTH_ROW); 
+    return res;
+  }
+
+  function chooseMouth(uint mouth) public pure returns (string[ROW_LENGTH] memory) {
+    if (mouth == 0) {
+      string[ROW_LENGTH] memory res = [" ", " ", " ", unicode"│", unicode"─", unicode"─", unicode" ", " ", unicode"│", " ", " ", " "];
+      return res;
+    }
+
+    string[ROW_LENGTH] memory defaultRes = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "];
+    return defaultRes;
+  }
+
+  function drawPunk() public pure returns (string[ROW_LENGTH * ROW_COUNT] memory) {
+    string[ROW_LENGTH * ROW_COUNT] memory base = [
+      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+      " ", " ", " ", unicode"┌",  unicode"─",  unicode"─",  unicode"─",  unicode"─",  unicode"┐", " ", " ", " ",
+      " ", " ", " ", unicode"│", " ", " ", " ", " ", unicode"├", unicode"┐", " ", " ",
+      " ", " ", " ", unicode"│", " ", " ", " ", " ", unicode"└", unicode"│", " ", " ",
+      " ", " ", " ", unicode"│", " ", " ", " ", " ", unicode"└", unicode"┘", " ", " ",
+      " ", " ", " ", unicode"│", " ", " ", " ", " ", unicode"│", " ", " ", " ",
+      " ", " ", " ", unicode"│", " ", " ", " ", " ", unicode"│", " ", " ", " ",
+      " ", " ", " ", unicode"│", " ", " ", " ", " ", unicode"│", " ", " ", " ",
+      " ", " ", " ", unicode"└", unicode"─", unicode"─", unicode"┘", " ", unicode"│", " ", " ", " ",
+      " ", " ", " ", " ", " ", unicode"│", " ", " ", unicode"│", " ", " ", " ",
+      " ", " ", " ", " ", " ", unicode"│", " ", " ", unicode"│", " ", " ", " "
+    ];
+
+    string[MOUTH_LENGTH] memory mouth = chooseMouth(0);
+
+    return drawMouth(base, mouth);
   }
 }
--- test/Punk.js
+++ test/Punk.js
@@ -1,13 +1,32 @@
 const Punk = artifacts.require("Punk");
 
 contract("Punk test", async (accounts) => {
-  it("should do stuff", async () => {
+  // it("should splice", async () => {
+  //   let instance = await Punk.deployed();
+  //   let spliced = await instance.splice(
+  //     ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  //     ["a", "a", "a"],
+  //     0
+  //   );
+  //   assert.deepEqual(spliced.slice(0, 3), ["a", "a", "a"]);
+  // });
+
+  it("should draw a punk", async () => {
     let instance = await Punk.deployed();
-    let spliced = await instance.splice(
-      ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-      ["a", "a", "a"],
-      0
-    );
-    assert.deepEqual(spliced.slice(0, 3), ["a", "a", "a"]);
+    let punk = await instance.drawPunk();
+    assert.deepEqual(punk, [
+      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+      " ", " ", " ", "┌", "─", "─", "─", "─", "┐", " ", " ", " ",
+      " ", " ", " ", "│", " ", " ", " ", " ", "├", "┐", " ", " ",
+      " ", " ", " ", "│", " ", " ", " ", " ", "└", "│", " ", " ",
+      " ", " ", " ", "│", " ", " ", " ", " ", "└", "┘", " ", " ",
+      " ", " ", " ", "│", " ", " ", " ", " ", "│", " ", " ", " ",
+      " ", " ", " ", "│", "─", "─", " ", " ", "│", " ", " ", " ",
+      " ", " ", " ", "│", " ", " ", " ", " ", "│", " ", " ", " ",
+      " ", " ", " ", "└", "─", "─", "┘", " ", "│", " ", " ", " ",
+      " ", " ", " ", " ", " ", "│", " ", " ", "│", " ", " ", " ",
+      " ", " ", " ", " ", " ", "│", " ", " ", "│", " ", " ", " "
+    ]);
   });
 });
