53acbd4eb3693f07277a27323d44563c1c5b9e9f
Nassredean Nasseri <dean@nasseri.io>
2021-03-06 17:59:18 -0500

    fix test

--- test/Punk.js
+++ test/Punk.js
@@ -3,7 +3,13 @@ const Punk = artifacts.require("Punk");
 contract("Punk test", async accounts => {
   it("should do stuff", async () => {
     let instance = await Punk.deployed();
-    // let balance = await instance.getBalance.call(accounts[0]);
-    assert.equal(1, 2);
+    let spliced = await instance.splice(
+      ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      ['a', 'a', 'a'],
+      0
+    );
+    assert.equal(spliced[0], 'a');
+    assert.equal(spliced[1], 'a');
+    assert.equal(spliced[2], 'a');
   });
 });
