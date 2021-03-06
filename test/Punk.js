3eb267a390fc7e4fbfc736066083a786f36280d9
kyle <kylelholzinger@gmail.com>
2021-03-06 18:11:04 -0500

    [KH] prettier and deepEqual

--- test/Punk.js
+++ test/Punk.js
@@ -1,15 +1,13 @@
 const Punk = artifacts.require("Punk");
 
-contract("Punk test", async accounts => {
+contract("Punk test", async (accounts) => {
   it("should do stuff", async () => {
     let instance = await Punk.deployed();
     let spliced = await instance.splice(
-      ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-      ['a', 'a', 'a'],
+      ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+      ["a", "a", "a"],
       0
     );
-    assert.equal(spliced[0], 'a');
-    assert.equal(spliced[1], 'a');
-    assert.equal(spliced[2], 'a');
+    assert.deepEqual(spliced.slice(0, 3), ["a", "a", "a"]);
   });
 });
