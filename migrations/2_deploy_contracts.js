efcb212de75064857203b15316f426871242c2ca
kyle <kylelholzinger@gmail.com>
2021-03-21 18:45:28 -0400

    [KH] use rinkeby maybe

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const AsciiPunks = artifacts.require('AsciiPunks');
 const AsciiPunkFactory = artifacts.require('AsciiPunkFactory');
 
 module.exports = function(deployer) {
-  deployer.deploy(AsciiPunks);
   deployer.deploy(AsciiPunkFactory);
+  deployer.deploy(AsciiPunks);
 }
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require("fs");
+require("dotenv").config();
 
 module.exports = {
   /**
@@ -43,10 +43,10 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-     websockets: true
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      websockets: true,
     },
     // Another network with more advanced options...
     // advanced: {
@@ -59,14 +59,16 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          process.env.INFURA_RINKEBY_URL
+        ),
+      network_id: 4,
+      gas: 10000000000,
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -85,12 +87,12 @@ module.exports = {
     solc: {
       version: "^0.8.0",
       settings: {
-       optimizer: {
-         enabled: false,
-         runs: 200
-       },
-       evmVersion: "byzantium"
-      }
-    }
-  }
+        optimizer: {
+          enabled: false,
+          runs: 200,
+        },
+        evmVersion: "byzantium",
+      },
+    },
+  },
 };
